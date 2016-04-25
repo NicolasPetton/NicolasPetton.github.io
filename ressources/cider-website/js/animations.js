1ffa37cd0a56431a47387dd959a4b90956af277d
Nicolas Petton <nicolas@petton.fr>
2016-04-25 14:04:13 +0200

    cider website update

--- ressources/cider-website/js/animations.js
+++ ressources/cider-website/js/animations.js
@@ -13,16 +13,23 @@ function insertContent(span, callback) {
 	insertTextContent(text, span, callback);
 }
 
-function insertTextContent(text, span, callback) {
+function insertTextContent(text, span, callback, inString) {
 	if (text.length) {
 		var nextChar = text.pop();
 		setTimeout(function() {
 			if(nextChar === ' ') {
 				nextChar = '&nbsp;';
+			} else if(nextChar === '(' || nextChar === ')') {
+				nextChar = '<span class="paren">' + nextChar + '</span>';
+			} else if(nextChar === '"') {
+				inString = !inString;
+			}
+			if (inString || nextChar === '"') {
+				nextChar = '<span class="string">' + nextChar + '</span>';
 			}
 			$('.caret').removeClass('animated');
 			span.html(span.html() + nextChar);
-			insertTextContent(text, span, callback);
+			insertTextContent(text, span, callback, inString);
 		}, 100);
 	} else {
 		setTimeout(callback, 160);
@@ -34,7 +41,7 @@ function insertResult(span, callback) {
 	span.text('"Cider rocks!"');
 	$('.first .caret').hide();
 	$('.second').show();
-	setTimeout(restartAnimation, 2000);
+	// setTimeout(restartAnimation, 2000);
 }
 
 function restartAnimation() {
